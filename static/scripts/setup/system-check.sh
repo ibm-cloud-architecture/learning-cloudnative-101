bda08e9c8ff26bd60e21001eb99f5acb294006d0
Bryan Kribbs <bakribbs@us.ibm.com>
2020-04-28 14:43:29 -0500

    Updated package.json

--- static/scripts/setup/system-check.sh
+++ static/scripts/setup/system-check.sh
@@ -1,3 +1,67 @@
 #!/bin/bash
 
 echo 'Running System Checks'
+
+if hash ibmcloud 2>/dev/null
+then
+    printf '\xE2\x9C\x85 IBM Cloud CLI \n'
+else
+    printf '\xE2\x9D\x8C IBM Cloud CLI \n'
+fi
+
+if hash git 2>/dev/null
+then
+    printf '\xE2\x9C\x85 Git CLI \n'
+else
+    printf '\xE2\x9D\x8C Git CLI \n'
+fi
+
+if hash crc 2>/dev/null
+then
+    printf '\xE2\x9C\x85 CRC \n'
+else
+    printf '\xE2\x9D\x8C CRC \n'
+fi
+
+if hash minikube 2>/dev/null
+then
+    printf '\xE2\x9C\x85 Minikube \n'
+else
+    printf '\xE2\x9D\x8C Minikube \n'
+fi
+
+if hash kubectl 2>/dev/null
+then
+    printf '\xE2\x9C\x85 Kubernetes CLI \n'
+else
+    printf '\xE2\x9D\x8C Kubernetes CLI \n'
+fi
+
+if hash oc 2>/dev/null
+then
+    printf '\xE2\x9C\x85 Openshift CLI \n'
+else
+    printf '\xE2\x9D\x8C Openshift CLI \n'
+fi
+
+if hash tkn 2>/dev/null
+then
+    printf '\xE2\x9C\x85 Tekton CLI \n'
+else
+    printf '\xE2\x9D\x8C Tekton CLI \n'
+fi
+
+if hash argocd 2>/dev/null
+then
+    printf '\xE2\x9C\x85 Argo CLI \n'
+else
+    printf '\xE2\x9D\x8C Argo CLI \n'
+fi
+
+
+if hash crc 2>/dev/null
+then
+    printf '\xE2\x9C\x85 CRC CLI \n'
+else
+    printf '\xE2\x9D\x8C CRC CLI \n'
+fi
